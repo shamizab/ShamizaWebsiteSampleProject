6a0d50d5b872490c026746059efbb7852eb1f7f2
shamizab <146303580+shamizab@users.noreply.github.com>
2024-04-24 01:59:31 +0000

    add javascript

---
+++ menu.js
@@ -0,0 +1,25 @@
+
+
+
+
+function onPageLoad(){
+const Button = document.querySelector(".nav-button");
+const Menu = document.querySelector(".navigation");
+
+Button.addEventListener("click", function(){
+    if (Menu.classList.contains("open");
+
+
+    }
+    else{
+        Menu.classList.add("open")
+    }
+
+})
+
+
+}
+
+
+
+window.addEventListener("load", onPageLoad)

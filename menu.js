7796adf3501b9b562d763bc4708906bbe71d56ab
shamizab <146303580+shamizab@users.noreply.github.com>
2024-04-24 02:21:59 +0000

    adding menu bars

--- menu.js
+++ menu.js
@@ -1,25 +1,15 @@
+function onPageLoad() {
+    const Button = document.querySelector(".nav-button");
+    const Menu = document.querySelector(".navigation");
 
-
-
-
-function onPageLoad(){
-const Button = document.querySelector(".nav-button");
-const Menu = document.querySelector(".navigation");
-
-Button.addEventListener("click", function(){
-    if (Menu.classList.contains("open");
-
-
-    }
-    else{
-        Menu.classList.add("open")
-    }
-
-})
-
-
+    Button.addEventListener("click", function() {
+        if( Menu.classList.contains("open") ) {
+            Menu.classList.remove("open");
+        }
+        else {
+            Menu.classList.add("open");
+        }
+    } )
 }
 
-
-
-window.addEventListener("load", onPageLoad)
+window.addEventListener("load", onPageLoad )
